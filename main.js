53a45d911bd1de6597187cb073ee7aa97110c7ab
Giash ITH <89529541+Giash1@users.noreply.github.com>
2021-09-24 11:15:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML="Paragrap changes.";
+    }
+function myFunction() {
+    document.getElementById("demo3").innerHTML="Hello World";
+    document.getElementById("demo4").innerHTML="How are you?";
+}
